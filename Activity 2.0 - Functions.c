ad8177283d4ab57b23216f899ebd3fad6672a2df
Goodhabt <89273215+Goodhabt@users.noreply.github.com>
2022-04-18 23:52:26 +0800

    Add files via upload

---
+++ Activity 2.0 - Functions.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void CalcGrade(int n1, int n2, int n3, int n4, int n5) {
+    int total, average;
+    total = n1 + n2 + n3 + n4 + n5;
+    average = total / 5;
+    printf("\nAverage: %d\n", average);
+}
+int main() {
+    int n1, n2, n3, n4, n5;
+    printf("Enter 5 numbers:\n");
+    scanf("%d%d%d%d%d", &n1, &n2, &n3, &n4, &n5);
+    CalcGrade(n1, n2, n3, n4, n5);
+    return 0;
+}
